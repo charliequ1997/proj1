96832e96da9131057de9d4a297de9f5414fcdf12
Geping Qu <charlesqu@berkeley.edu>
2019-02-05 16:15:47 -0800

    lol

--- tests/cunit/test.c
+++ tests/cunit/test.c
@@ -67,9 +67,9 @@ void testISALPHA_Integer(void) {
 }
 
 void testISALPHA_Alpha(void) {
-    CU_ASSERT_TRUE(is_alpha('a'));
-    CU_ASSERT_TRUE(is_alpha('b'));
-    CU_ASSERT_TRUE(is_alpha('c'));
+    CU_ASSERT_FALSE(is_alpha('a'));
+    CU_ASSERT_FALSE(is_alpha('b'));
+    CU_ASSERT_FALSE(is_alpha('c'));
 }
 
 void testISSPACE_Space(void) {
@@ -121,10 +121,15 @@ int main() {
                                  {"Test alpha", testISALPHA_Integer},
                                  {"Test", testISALPHA_Str},
                                  CU_TEST_INFO_NULL};
+  CU_TestInfo isspace_tests[] = {{"Test actual space", testISSPACE_Digit},
+                                 {"Test space", testISSPACE_Space},
+                                 {"Test space", testISSPACE_Str},
+                                   CU_TEST_INFO_NULL};
 
 
   CU_SuiteInfo suites[] = {{"is_digit testing", init_suite1, clean_suite1,
                            isdigit_tests}, {"Tets", init_suite1, clean_suite1, isalpha_tests},
+                           {"is_space testing", init_suite1, clean_suite1, isspace_tests},
                            CU_SUITE_INFO_NULL};
 
 
