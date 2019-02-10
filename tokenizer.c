dc828745563591c605282633b596f4e54d67a2dd
Geping Qu <charlesqu@berkeley.edu>
2019-02-10 14:49:26 -0800

    lol

--- tokenizer.c
+++ tokenizer.c
@@ -357,31 +357,7 @@ size_t SelectToken(char* buffer,
   } else if (buffer[size_read] == '\'') {  // characters and some errors
 
     /* YOUR CODE HERE */
-    if ((size_read + 2 >= size) || (size_read + 3 >= size) || (size_read + 4 >= size)) {
-        return size_read;
-    }
-
-    if (isprint(buffer[size_read + 1]) && buffer[size_read + 2] == '\''){
-        t = create_token(filename);
-        t->linenum = *linenum;
-        t->type = TOKEN_CHARACTER;
-        t->data.character = buffer[size_read + 1];
-        size_read = size_read + 3;
-    } else if (replace_escape_in_character(buffer + size_read + 1) != -1 && buffer[size_read + 3] == '\'') {
-        t = create_token(filename);
-        t->linenum = *linenum;
-        t->type = TOKEN_CHARACTER;
-        t->data.character = replace_escape_in_character(buffer + size_read + 1);
-        size_read = size_read + 4;
-    } else {
-          int total =
-              generate_character_error(&t, buffer, size_read, size, *linenum, filename);
-          if (total == 0) {
-            return size_read;
-          } else {
-            size_read += total;
-      }
-  }
+    
 
   } else if (buffer[size_read] == '"') {  // strings and some errors
     size_t str_len = 1;
